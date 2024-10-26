455b3ec5d31ffa4d64abc6c499f045ed1be2d554
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-26 11:54:03 +0100

    add activity comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ActivityComponent } from './components/activity/activity.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+     {
+        path:"activity",
+        component: ActivityComponent
+     }
+];
