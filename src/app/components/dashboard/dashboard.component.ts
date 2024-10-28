b56ef5eb312278d6a22f97bdb064e09ecfb1efcd
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-28 23:49:27 +0100

    add chart Js

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { UserService } from '../../service/user.service';
+import Chart, { CategoryScale } from 'chart.js/auto';
 
+
+Chart.register(CategoryScale);
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -16,6 +19,9 @@ export class DashboardComponent {
   workouts:any;
   activities:any;
 
+
+  @ViewChild('workoutLineChart') private workoutLineChartRef: ElementRef;
+  @ViewChild('ActivitiesLineChart') private ActivitiesLineChartRef: ElementRef;
   constructor(private userService:UserService) { }
   ngOnInit(): void {
     this.getStats();
@@ -28,6 +34,34 @@ export class DashboardComponent {
       this.workouts = res.workouts;
       this.activities = res.activities;
       console.log(res);
+      if(this.workoutLineChartRef || this.ActivitiesLineChartRef){
+        this.createLineChart();
+      }
+    });
+  }
+
+  createLineChart(){
+    const workoutCtx=this.workoutLineChartRef.nativeElement.getContext('2d');
+    const activitiesCtx=this.ActivitiesLineChartRef.nativeElement.getContext('2d');
+   
+
+    new Chart(workoutCtx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
     });
   }
   getStats(){
